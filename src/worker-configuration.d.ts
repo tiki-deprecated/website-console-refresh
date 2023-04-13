1615386859a7ed23b8dd9454a5a4c6440a565053
Mike Audi <mike@mytiki.com>
2023-04-13 15:54:55 -0500

    feat!: move out of console
    no changes. just moving the existing code

---
+++ src/worker-configuration.d.ts
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) TIKI Inc.
+ * MIT license. See LICENSE file in root directory.
+ */
+interface Env {
+  L0_AUTH_URL: string
+  COOKIE: string
+  ORIGIN: string
+  SECURE: string
+}
